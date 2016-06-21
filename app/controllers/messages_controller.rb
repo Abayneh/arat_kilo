f8a79bf03f67978653c34bec762789e6ff6677e3
Abayneh <tabayneh@gmail.com>
2016-06-21 22:51:34 +0000

    Show message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,11 @@
 class MessagesController < ApplicationController
   def index
+    # list all messages
+    @messages = Message.all.order("created_at DESC")
+  end
+  
+  def show
+    @message = Message.find(params[:id])
   end
   
   def new
