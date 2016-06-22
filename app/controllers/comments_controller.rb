2353d371d864112bcfd23a1a058f418366eb854d
Abayneh <tabayneh@gmail.com>
2016-06-22 00:33:27 +0000

    Comments Model, conteroller and view files

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+  
+  def create
+    @message = Message.find(params[:message_id])
+    @comment = @message.comments.create(comment_params)
+    @comment.user_id = current_user.user_id
+    
+    if @comment.save
+      redirect_to message_path(@message)
+    else
+      render 'new'
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private 
+  
+  def comment_params
+    params.requre(:comment).permit(:content)
+  end
+end
